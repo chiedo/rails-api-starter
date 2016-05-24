3643d3146dc3b6d85e659464a83d8feb1fcded04
Chiedo <chiedo@chie.do>
2016-05-24 09:40:05 -0400

    No more test generators

--- config/application.rb
+++ config/application.rb
@@ -23,14 +23,9 @@ module ExampleApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # Decides what generators to use
+    # Decides what generators to use. No generators for tests
     config.generators do |g|
-      g.test_framework :rspec
-      g.view_specs false
-      g.helper_specs false
-      g.request_specs false
-      g.controller_specs true
-      g.routing_specs true
+      g.test_framework nil
     end
 
     # Allow for Rack Cors
